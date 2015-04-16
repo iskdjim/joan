0b78d82b3d53cfe86d87e86024e7986650cdfad4
Michael Pilz <michael.pilz@student.tugraz.at>
2015-04-16 14:28:05 +0200

    better perform

--- Trying/Perfomance Test/app.js
+++ Trying/Perfomance Test/app.js
@@ -21,3 +21,33 @@ function getJsonData(){
 
     
 }    
+
+
+function stats(){
+	        var stats = new Stats();
+			stats.setMode(0); // 0: fps, 1: ms
+			
+			// align top-left
+			stats.domElement.style.position = 'absolute';
+			stats.domElement.style.left = '0px';
+			stats.domElement.style.top = '0px';
+
+			document.body.appendChild( stats.domElement );
+
+			
+			return stats;
+}
+
+function mstats(){
+	       
+			var mstats = new Stats();
+			stats.setMode(1); //
+			
+			mstats.domElement.style.position = 'fixed';
+			mstats.domElement.style.right  = '0px';
+			mstats.domElement.style.top   = '0px';
+
+			document.body.appendChild( mstats.domElement );
+			
+			return mstats;
+}
