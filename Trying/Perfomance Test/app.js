ef4c497a8d02479f5e145461b986637bbc3d84b1
Michael Pilz <michael.pilz@student.tugraz.at>
2015-05-05 13:24:34 +0200

    refactor

--- Trying/Perfomance Test/app.js
+++ Trying/Perfomance Test/app.js
@@ -1,53 +1,51 @@
-function getData(){
-    	  console.log("getData");
-    
-    	  $.get('api/request_handler.php', {} , function(data) {
-    	  	console.log(data);
-                if(data){
-                	return data;
-                }   
-           }, 'json');
+var mili_secs = 0;
 
+function getData(){
+  $.get('api/request_handler.php', {} , function(data) {
+    if(data){
+      return data;
+    }   
+  }, 'json');
 }
     
 function getJsonData(){
-
-		return "sd";
-		$.getJSON( "api/data.json", function( data ) {
-	
-			return "data";
-			
-		});
-
-    
+  return "test";
+  $.getJSON( "api/data.json", function( data ) {
+    return "data";
+  }); 
 }    
 
-
 function stats(){
-	        var stats = new Stats();
-			stats.setMode(0); // 0: fps, 1: ms
+  var stats = new Stats();
+  stats.setMode(0); // 0: fps, 1: ms
 			
-			// align top-left
-			stats.domElement.style.position = 'absolute';
-			stats.domElement.style.left = '0px';
-			stats.domElement.style.top = '0px';
+  // align top-left
+  stats.domElement.style.position = 'absolute';
+  stats.domElement.style.left = '0em';
+  stats.domElement.style.top = '0em';
 
-			document.body.appendChild( stats.domElement );
-
-			
-			return stats;
+  document.body.appendChild( stats.domElement );
+  return stats;
 }
 
 function mstats(){
-	       
-			var mstats = new Stats();
-			stats.setMode(1); //
-			
-			mstats.domElement.style.position = 'fixed';
-			mstats.domElement.style.right  = '0px';
-			mstats.domElement.style.top   = '0px';
+  var mstats = new Stats();
+  stats.setMode(1); 
+  
+  mstats.domElement.style.position = 'fixed';
+  mstats.domElement.style.right  = '0em';
+  mstats.domElement.style.top   = '0em';
 
-			document.body.appendChild( mstats.domElement );
+  document.body.appendChild( mstats.domElement );
 			
-			return mstats;
+  return mstats;
+}
+
+function mSecStats(){
+  var mili_secs_interval;
+  mili_secs_interval = setInterval(function(){
+    mili_secs++; 
+  }, 1);
+  return 1;
+
 }
