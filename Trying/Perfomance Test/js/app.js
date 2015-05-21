b782f4702f1f120c9e2001defd56f3869d331268
Michael Pilz <michael.pilz@student.tugraz.at>
2015-05-21 17:06:09 +0200

    refactor

--- Trying/Perfomance Test/js/app.js
+++ Trying/Perfomance Test/js/app.js
@@ -89,3 +89,57 @@ function showStats(stats_data_fps,stats_data_ms){
   $('#average_ms').html(ms_av);
   $('#sum_ms').html(ms_av*$('#iterations').val());
 }
+
+function prepareData(data,type,range,simplify_options){
+  var ranged_points = new Array();
+  var range_counter = 0;	
+  var x_range = 0; // some day its the time value  
+  var high_quality = false;
+  $.each(data, function(i,val){
+    if(range < range_counter){
+      return;
+    }
+    ranged_points.push({x:x_range, y:(val.chanels[0].value/50), time:val.time});	
+    range_counter++;
+    x_range += 0.02;
+  })
+  if(simplify_options[0]){
+  	data = simplify(ranged_points, simplify_options[1], high_quality);
+  }else{
+    data = ranged_points;
+  }  
+  	
+  return data;
+
+}
+
+
+function drawSvgLines(prepare_data, target){
+  var shift_counter = 0;	
+  var line_points = new Array(new Array(0,0));
+  
+  $.each(prepare_data, function(i,val){        
+    shift_counter++;
+    line_points.push(new Array( val.x, val.y));
+    if(shift_counter > 1){
+     line_points.shift();
+    }
+							
+    x1 = line_points[0][0];
+    y1 = line_points[0][1];
+    x2 = line_points[1][0];
+    y2 = line_points[1][1];
+
+    var newLine = document.createElementNS('http://www.w3.org/2000/svg','line');
+    newLine.setAttribute('id','time_'+val.time);
+    newLine.setAttribute('x1',x1);
+    newLine.setAttribute('y1',y1);
+    newLine.setAttribute('x2',x2);
+    newLine.setAttribute('y2',y2);
+    newLine.setAttribute('style','stroke:rgb(0,0,0)');
+
+    target.append(newLine);	
+  					  
+  })
+
+}
